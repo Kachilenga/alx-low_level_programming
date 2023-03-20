811309511d27850a24a695595995f1ade550281a
kachilenga <kachilengabanda@gmail.com>
2023-03-20 22:12:18 +0000

    first question

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,13 @@
+#include "dog.h"
+/**
+ * init_dog - initialize dog
+ * @struct dog - Structure of a dog
+ * @d: pointer to dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+}
+
