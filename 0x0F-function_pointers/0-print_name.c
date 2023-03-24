a488775242afc4e5d0b6aa8e083e6d1d6cd880d2
kachilenga <kachilengabanda@gmail.com>
2023-03-24 19:43:16 +0000

    first 3

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - function to print the name
+ * @name: holds the name
+ * @f: pointer to function
+ * Return: nothing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	f(name);
+}
