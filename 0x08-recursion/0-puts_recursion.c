b74da00d280e17cf9ea52e2543c99f2187f54e52
kachilenga <kachilengabanda@gmail.com>
2023-06-08 09:20:51 +0200

    readme and puts_recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * puts_recursion : function that prints the string
+ * @s - variable holding the string
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(*s);
+	puts("\n");
+}
