826609fe2e86a579870485317435b56b08cfef1e
kachilenga <kachilengabanda@gmail.com>
2023-06-08 09:45:53 +0200

    readme and puts_recursion updated

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 #include <stdio.h>
 
-/*
- * puts_recursion : function that prints the string
- * @s - variable holding the string
+/**
+ * _puts_recursion -function that prints the string followed by new line
+ * @s: variable holding the string
  */
 
 void _puts_recursion(char *s)
