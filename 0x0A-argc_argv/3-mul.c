295b37da229b4d7adade222ffa01c8d90c6b4145
kachilenga <kachilengabanda@gmail.com>
2023-06-09 22:20:05 +0200

    args

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: Used to count the args
+ * @argv: Used to print the args
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	if (argc == 1 || argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		i = atoi(argv[1]) * atoi(argv[2]);
+		printf("%d\n", i);
+	}
+	return (0);
+}
