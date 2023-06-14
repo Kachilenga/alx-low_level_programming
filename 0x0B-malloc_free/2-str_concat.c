0397c21b59037d6ac7901844270fccfe56f28551
kachilenga <kachilengabanda@gmail.com>
2023-06-15 00:06:15 +0200

    update conca

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,6 +1,13 @@
 #include "main.h"
 #include <stdlib.h>
 
+/**
+ * str_concat - function to concatenate strings
+ * @s1: first variable
+ * @s2: second variable
+ * Return: NULL
+ */
+
 char *str_concat(char *s1, char *s2)
 {
 	int i, j;
