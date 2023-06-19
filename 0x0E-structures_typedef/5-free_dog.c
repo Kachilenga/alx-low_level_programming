ee786079ed7e5f66a30731513e4a733e7e0344ba
kachilenga <kachilengabanda@gmail.com>
2023-06-19 20:40:01 +0200

    5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - function to free the dogs
+ * @d: the name of the dog to be freed
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		free(d->name);
+		free(d->age);
+		free(d->owner);
+	}
+}
