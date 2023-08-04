a193f65c0e59814f2ccf29122c934ead11b7710b
kachilenga <kachilengabanda@gmail.com>
2023-08-04 11:10:26 +0200

    argv argc

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: Used to count the args
+ * @argv: Used to print the args
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	unsigned int i;
+
+	if (argc == 1 || argc < 2)
+	{
+		printf("0\n");
+		
+	}
+	else if (argc % 10
+	{
+
+	{
+		i = atoi(argv[1]) + atoi(argv[2]);
+		printf("%d\n", i);
+	}
+	return (0);
+}
